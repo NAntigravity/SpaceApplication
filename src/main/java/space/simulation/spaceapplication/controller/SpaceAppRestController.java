9a04bcea0b911e62c959fbbd5073ed0c871d8c50
NAntigrvity <amajski07@gmail.com>
2022-10-16 18:14:04 +0700

    [FIX][BACK][13427] - add SpaceAppViewController.java with method getMap() for explicit mapping for / endpoint (access data from Thymeleaf map.html template) - add @RestController annotation for SpaceAppRestController.java

--- src/main/java/space/simulation/spaceapplication/controller/SpaceAppRestController.java
+++ src/main/java/space/simulation/spaceapplication/controller/SpaceAppRestController.java
@@ -5,8 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import space.simulation.spaceapplication.game.ControlClass;
 
+@RestController
 public class SpaceAppRestController {
     public static ControlClass game;
 
