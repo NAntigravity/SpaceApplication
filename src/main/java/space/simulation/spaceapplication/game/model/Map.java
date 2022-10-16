b4db2b7e4e1382418a7ff3c341e1df9414f93c89
Anna Torgunakova <annatorgun@mail.ru>
2022-10-16 17:03:22 +0700

    [FT][DEV][BACK] add map generation algorithm - add generateEmptyMap() in Map.java (base map generation algorithm) - add Tile.java

--- src/main/java/space/simulation/spaceapplication/game/model/Map.java
+++ src/main/java/space/simulation/spaceapplication/game/model/Map.java
@@ -1,4 +1,36 @@
 package space.simulation.spaceapplication.game.model;
 
-public class Map {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Vector;
+
+public class Map implements Serializable {
+    @Getter
+    @Setter
+    private Integer height;
+    @Getter
+    @Setter
+    private Integer width;
+    @Getter
+    @Setter
+    private Vector<Vector<Tile>> tiles;
+
+    public Map(int x, int y) {
+        height = y;
+        width = x;
+        generateEmptyMap();
+    }
+
+    private void generateEmptyMap() {
+        tiles = new Vector<>();
+        for (int i = 0; i < height; i++) {
+            Vector<Tile> tileVector = new Vector<>();
+            for (int j = 0; j < width; j++) {
+                tileVector.add(new Tile());
+            }
+            tiles.add(tileVector);
+        }
+    }
 }
--- src/main/java/space/simulation/spaceapplication/game/model/Tile.java
+++ src/main/java/space/simulation/spaceapplication/game/model/Tile.java
@@ -1,4 +1,22 @@
 package space.simulation.spaceapplication.game.model;
 
-public class Tile {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+public class Tile implements Serializable {
+    @Getter
+    @Setter
+    protected Class tileType;
+
+    public Tile() {
+        super();
+        tileType = Tile.class;
+    }
+
+    public Tile(Class tileType) {
+        super();
+        this.tileType = tileType;
+    }
 }
