97d0281224116deda368ec48fa61c9ac67299903
NAntigrvity <amajski07@gmail.com>
2022-10-16 20:30:13 +0700

    [FIX][BACK] add missing fields to Entity.java

--- src/main/java/space/simulation/spaceapplication/game/model/Entity.java
+++ src/main/java/space/simulation/spaceapplication/game/model/Entity.java
@@ -20,5 +20,13 @@ public abstract class Entity {
     @Setter
     protected Integer coordinateY;
 
+    @Getter
+    @Setter
+    protected Class entityType;
+
+    @Getter
+    @Setter
+    protected boolean isDead = false;
+
     public abstract void existOneTick();
 }
