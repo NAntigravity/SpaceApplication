eb2a50e93233eb6b62910f57155697209987bf4d
Anna Torgunakova <annatorgun@mail.ru>
2022-10-16 23:19:39 +0700

    [FT][FRONT][13439] add star visualization - add tile for star - rework logic of showing tiles

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,7 +1,10 @@
 function switchImageByID(tile, ID) {
     switch (ID) {
-        case "space.simulation.oop.game.model.Tile":
+        case "space.simulation.spaceapplication.game.model.Tile":
             tile.className = "tile";
+            break;
+        case "space.simulation.spaceapplication.game.model.celestial.bodies.Star":
+            tile.className = "star";
             break;
     }
 }
@@ -15,9 +18,10 @@ async function mapCreation() {
     let response = await fetch('http://localhost:8080/spaceAPI');
     let json = await response.json();
 
-    w = json.width;
-    h = json.height;
-    tiles = json.tiles;
+    w = json.map.width;
+    h = json.map.height;
+    tiles = json.map.tiles;
+    ent = json.map.entities;
 
     map = document.getElementById("field");
 
@@ -26,7 +30,7 @@ async function mapCreation() {
         col.classList.add("row");
         for (let j = 0; j < w; j++) {
             let img = document.createElement("img");
-            switchImageByID(img, tiles[i][j].tileType[0]);
+            switchImageByID(img, tiles[i][j].tileType);
 
             img.setAttribute('X', j);
             img.setAttribute('Y', i);
